01b1efea08ba9a8a80b17444bffc63997ab997bc
cranix <cranix@hackerspace.pl>
2017-11-05 01:32:27 +0100

    Added mix parameter

--- plugins/Super_Saw/SuperSaw.cpp
+++ plugins/Super_Saw/SuperSaw.cpp
@@ -8,7 +8,7 @@ START_NAMESPACE_DISTRHO
 class SuperSaw : public Plugin
 {
 public:
-  SuperSaw() : Plugin(1,0,0){
+  SuperSaw() : Plugin(2,0,0){
         adainit();
   }
 
@@ -66,36 +66,53 @@ protected:
   }
   void setParameterValue(uint32_t index, float value) override
   {
-    detune = value;
+    if (index == 0) {
+      detune = value;
+    } else if (index == 1) {
+      mix = value;
+    }
   }
 
   float getParameterValue(uint32_t index) const override
   {
-    return detune;
+    if (index == 0) {
+      return detune;
+    } else if (index == 1) {
+      return mix;
+    }
 
   }
 
   void initParameter(uint32_t index, Parameter& parameter) override {
-    parameter.hints      = kParameterIsAutomable;
-    parameter.name       = "Detune";
-    parameter.symbol     = "detune";
-    parameter.ranges.min = 0.0f;
-    parameter.ranges.max = 1.0f;
-    parameter.ranges.def = 0.5f;
+    if (index == 0) { /*Detune*/
+      parameter.hints      = kParameterIsAutomable;
+      parameter.name       = "Detune";
+      parameter.symbol     = "detune";
+      parameter.ranges.min = 0.0f;
+      parameter.ranges.max = 1.0f;
+      parameter.ranges.def = 0.5f;
+    } else if (index == 1) { /*Mix*/
+      parameter.hints      = kParameterIsAutomable;
+      parameter.name       = "Mix";
+      parameter.symbol     = "mix";
+      parameter.ranges.min = 0.0f;
+      parameter.ranges.max = 1.0f;
+      parameter.ranges.def = 0.5f;
+    }
   }
 
 
   void run(const float** inputs, float** outputs, uint32_t frames) override {
     uint32_t i;
     for(i=0;i<frames;i++){
-      outputs[0][i] = Super_Saw(phase,1000.0,detune,0.5,getSampleRate())/50.0;
+      outputs[0][i] = Super_Saw(phase,1000.0,detune,mix,getSampleRate())/50.0;
       phase++;
     }
   }
 private:
   float phase=0;
   float detune;
-
+  float mix;
 };
 
 Plugin* createPlugin()
