ae36e6d0bfb49a740402ef81b7e6d4fdcfd8411a
cranix <cranix@hackerspace.pl>
2017-12-09 11:50:45 +0100

    Changed URL to point to github repo

--- plugins/Super_Saw/DistrhoPluginInfo.h
+++ plugins/Super_Saw/DistrhoPluginInfo.h
@@ -2,7 +2,7 @@
 #define DISTRHO_PLUGIN_INFO_H_INCLUDED
 
 #define DISTRHO_PLUGIN_NAME  "Super Saw"
-#define DISTRHO_PLUGIN_URI   "http://example.org/Super_Saw"
+#define DISTRHO_PLUGIN_URI   "https://github.com/cranixx/Super_Saw_DPF"
 #define DISTRHO_PLUGIN_NUM_INPUTS 1
 #define DISTRHO_PLUGIN_NUM_OUTPUTS 1
 #define DISTRHO_PLUGIN_IS_SYNTH   1
